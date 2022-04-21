bae52a21639a95329d4b2e5ba9f1d7ecf78eb7e8
Burak Doğan <76884187+burakdogangazi@users.noreply.github.com>
2022-04-21 10:08:30 +0300

    Update forms.py

--- user/forms.py
+++ user/forms.py
@@ -11,7 +11,7 @@ class RegisterForm(forms.Form):
     password = forms.CharField(max_length=20,label = "Parola",widget= forms.PasswordInput)
     confirm = forms.CharField(max_length=20,label="Parolayı Doğrula",widget=forms.PasswordInput)
     
-    def clean(self): #override ettik
+    def clean(self):
         username = self.cleaned_data.get("username")
         password = self.cleaned_data.get("password")
         confirm = self.cleaned_data.get("confirm")
